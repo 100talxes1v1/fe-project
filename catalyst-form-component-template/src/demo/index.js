22fc2a675872c815f62a869a165a1e066dcfb3c6
yaangbo7 <yangbo7@100tal.com>
2020-06-27 22:45:24 +0800

    模板配置修改

--- catalyst-form-component-template/src/demo/index.js
+++ catalyst-form-component-template/src/demo/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 import Catalyst from '@xes/dh-catalyst';
 import '@xes/dh-catalyst/dist/catalyst.min.css';
-import mpUI from '@xes/dh-component-vue-mp-ui';
-import '@xes/dh-component-vue-mp-ui/dist/dh_component_vue_mp_ui.min.css';
-// import './assets/styles/main.scss'
 import App from './app.vue';
 import router from './router';
 Vue.use(ElementUI);
-Vue.use(mpUI);
 Vue.use(Catalyst);
 
 new Vue({
