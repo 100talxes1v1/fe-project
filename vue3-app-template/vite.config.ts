f6a67d6255c97ec3bb5e823405933f7fd2ef338d
robert <robert0609@163.com>
2021-04-11 21:22:09 +0800

    update

--- vue3-app-template/vite.config.ts
+++ vue3-app-template/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-
-const path = require('path');
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment'
+  },
   plugins: [
     vue(),
     vueJsx()
@@ -15,4 +18,16 @@ export default defineConfig({
       { find: "@", replacement: path.resolve(__dirname, 'src') }
     ],
   },
+  build: {
+    minify: true,
+    rollupOptions: {
+      output: {
+        namespaceToStringTag: false // close this option, so that router dynamic import will result "Cannot read property 'toStringTag' of undefined". reference: https://github.com/vitejs/vite/issues/1921
+      }
+    }
+  },
+  server: {
+    proxy: {
+    }
+  }
 });
