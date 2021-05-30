2eced1816489278d891453887f78d5ddfd4ce612
robert <robert0609@163.com>
2021-05-31 00:11:44 +0800

    update

--- vite-lib-template/vite.config.ts
+++ vite-lib-template/vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'MyLib'
+      name: '<%=libraryName%>'
     },
     minify: true,
     sourcemap: true,
